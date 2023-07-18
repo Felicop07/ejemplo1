5c5aa8ed6dc7663defec3ffce4c7963a65f404d6
Felicop07 <felicop07@gmail.com>
2023-07-17 22:56:17 -0500

    inicio

---
+++ My project (1)/Assets/Pixel Adventure 1/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public static bool damageSignal = false;
+    public static float damageTime = 0f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            damageSignal = true;
+            damageTime = 0f;
+        }
+    }
+    //public static void Lesslife()
+    //{
+    //    lifeManager.lifes -= 1;
+    //}
+}
---
+++ My project (1)/Assets/Pixel Adventure 1/Assets/Scripts/DetectGround.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DetectGround : MonoBehaviour
+{
+    public static bool checkGround;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Suelo"))
+        {
+            checkGround = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Suelo"))
+        {
+            checkGround = false;
+        }
+    }
+}
